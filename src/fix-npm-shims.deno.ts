fd282f703434edf5ffb6efe216c09028b14749f5
Roy Ivy III <rivy.dev@gmail.com>
2021-01-20 01:03:29 -0600

    wip! ~ overload sync/async iterable functions

--- src/fix-npm-shims.deno.ts
+++ src/fix-npm-shims.deno.ts
@@ -4,9 +4,11 @@
 
 // `deno run --allow-... PROG`
 
+// spell-checker:ignore (OS) Cygwin MSYS
 // spell-checker:ignore (shell/cmd) COMSPEC PATHEXT
 
-import { expandGlobSync } from 'https://deno.land/std@0.83.0/fs/mod.ts';
+import * as fs from 'https://deno.land/std@0.83.0/fs/mod.ts';
+import * as path from 'https://deno.land/std@0.83.0/path/mod.ts';
 
 const shimTemplate = `
 @setLocal
@@ -35,13 +37,93 @@ endLocal & goto #_undefined_# 2>NUL || title %COMSPEC% & "%_prog%" "\${binPath}"
 `;
 
 const isWinOS = Deno.build.os === 'windows';
-const pathSeparator = isWinOS ? '[\\/]' : '/';
-const pathListSeparator = isWinOS ? ';' : ':';
+// const pathSeparator = isWinOS ? /[\\/]/ : /\//;
+const pathListSeparator = isWinOS ? /;/ : /:/;
+// const paths = Deno.env.get('PATH')?.split(pathListSeparator) || [];
+const pathExtensions = (isWinOS && Deno.env.get('PATHEXT')?.split(pathListSeparator)) || [];
 
-const npmBinPath = './{kb,fix,djs}*';
-const files = Array.from(expandGlobSync(npmBinPath));
+// influenced by code from <https://github.com/npm/node-which/blob/master/which.js> (ISC License)
+// handle PATHEXT for Cygwin or MSYS?
 
-files.forEach((file) => console.log({ file }));
+type findFileOptions = {
+	paths?: readonly string[];
+	extensions?: readonly string[];
+};
 
-console.log({ mainModule: Deno.mainModule });
-console.log({ PATH: Deno.env.get('PATH') });
+async function* findFile(
+	name: string,
+	options: findFileOptions = {}
+): AsyncIterableIterator<string> {
+	const paths = options.paths
+		? options.paths
+		: (isWinOS ? ['.'] : []).concat(Deno.env.get('PATH')?.split(pathListSeparator) || []);
+	const extensions = options.extensions
+		? options.extensions
+		: (isWinOS && Deno.env.get('PATHEXT')?.split(pathListSeparator)) || [''];
+	for (const path_ of paths) {
+		for (const extension of extensions) {
+			const p = path.join(path_, name) + extension;
+			if ((await fs.exists(p)) && (isWinOS || ((await Deno.lstat(p)).mode || 0) & 0o111)) {
+				yield p;
+			}
+		}
+	}
+}
+
+function* findFileSync(name: string, options: findFileOptions = {}): IterableIterator<string> {
+	const paths = options.paths
+		? options.paths
+		: (isWinOS ? ['.'] : []).concat(Deno.env.get('PATH')?.split(pathListSeparator) || []);
+	const extensions = options.extensions
+		? options.extensions
+		: (isWinOS && Deno.env.get('PATHEXT')?.split(pathListSeparator)) || [''];
+	for (const path_ of paths) {
+		for (const extension of extensions) {
+			const p = path.join(path_, name) + extension;
+			if (fs.existsSync(p) && (isWinOS || (Deno.lstatSync(p).mode || 0) & 0o111)) {
+				yield p;
+			}
+		}
+	}
+}
+
+/**
+ *  Collect all sequence values into a promised array (`Promise<T[]>`)
+ */
+async function collect<T>(iterable: AsyncIterable<T> | Iterable<T>) {
+	const arr: T[] = [];
+	for await (const x of iterable) {
+		arr.push(x);
+	}
+	return arr;
+}
+
+/**
+ * Converts a (potentially infinite) sequence into a sequence of length `n`
+ */
+async function* take<T>(n: number, iterable: AsyncIterable<T> | Iterable<T>) {
+	for await (const x of iterable) {
+		if (n <= 0) {
+			break; // closes iterable
+		}
+		n--;
+		yield x;
+	}
+}
+
+// const npmBinPath = './{kb,fix,djs}*';
+// const files = Array.from(fs.expandGlobSync(npmBinPath));
+// files.forEach((file) => console.log({ file }));
+
+// console.log({ mainModule: Deno.mainModule });
+// console.log({ PATH: Deno.env.get('PATH') });
+// console.log({ paths, pathExtensions });
+
+// const files = await collect(
+// 	take(Infinity, findFile('npm', { extensions: ['', ...pathExtensions] }))
+// );
+// console.log({ files });
+
+const npmPath = await collect(take(1, findFile('npm')));
+
+console.log({ npmPath });
