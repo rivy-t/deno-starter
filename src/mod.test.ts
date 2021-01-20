e4fa699b96ab4062809a823dd141bdbbbc693bae
Roy Ivy III <rivy.dev@gmail.com>
2021-01-19 21:38:10 -0600

    fixup! refactor ~ revise code directory structure (use 'src' and 'test' directories)

--- src/mod.test.ts
+++ src/mod.test.ts
@@ -1,9 +1,9 @@
-import { assertEquals } from "../test/test_deps.ts";
-import { getHelloWorld } from "./mod.ts";
+import { assertEquals } from '../test/test_deps.ts';
+import { getHelloWorld } from './mod.ts';
 
 Deno.test({
-  name: "first test",
-  fn(): void {
-    assertEquals(getHelloWorld(), "\x1b[1mHello World\x1b[22m");
-  },
+	name: 'first test',
+	fn(): void {
+		assertEquals(getHelloWorld(), '\x1b[1mHello World\x1b[22m');
+	},
 });
