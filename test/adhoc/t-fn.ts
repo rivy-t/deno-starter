44b689d345fbdd8933a3a63ccf199b93a1698c34
Roy Ivy III <rivy.dev@gmail.com>
2021-03-29 21:21:56 -0500

    wip! improve adhoc test code to show more functionality

--- test/adhoc/t-fn.ts
+++ test/adhoc/t-fn.ts
@@ -9,6 +9,7 @@
 // console.log({ x });
 
 import * as F from '../../src/fn.ts'; // spell-checker:ignore unnest
+import type { ValueOrArray } from '../../src/fn.ts'; // spell-checker:ignore unnest
 import * as R from 'https://deno.land/x/ramda@v0.27.2/mod.ts';
 
 // const z = await F.collect(F.zip(F.range(10, Infinity), ['a', 'bb', 'ccc', 'dddd']));
@@ -25,7 +26,7 @@ const flat_e_m1 = await F.collect(F.flatten(e_m1));
 const flat_z_m1 = await F.collect(F.flatten(z_m1));
 console.log({ m1, e_m1, z_m1, flat_e_m1, flat_z_m1 });
 
-const y = [1, [2, [3, 4], [5, 6], 7], [8, 9], [[10, [11, 12]], 13]];
+const y: ValueOrArray<number>[] = [1, [2, [3, 4], [5, 6], 7], [8, 9], [[10, [11, 12]], 13]];
 
 const flatY = F.collectSync(F.flattenSync(y));
 const unNestY1 = await F.collect(F.unnest(1, y));
@@ -47,8 +48,8 @@ const sbl = Symbol('unique');
 let o = { 1: 'one', 2: 'two', sym: 10, [sbl]: 'symbol-here' };
 const set = new Set(['one', 2, 'help', {}]);
 const str = 'a string';
-const aa = await F.collect(F.map(double, m.values()));
-console.log({ m, aa });
+const double_m_vals = await F.collect(F.map(double, m.values()));
+console.log({ m, double_m_vals });
 
 const b = R.map(double, await F.toArray(F.flatten(y)));
 console.log({ b });
@@ -63,8 +64,10 @@ console.log({
 	first: await F.first(a),
 	tail: await F.collect(F.tail(a)),
 	last: await F.last(a),
-	take: await F.collect(F.take(2, a)),
-	drop: await F.collect(F.drop(2, a)),
+	firstN3: await F.collect(F.firstN(3, a)),
+	take2: await F.collect(F.take(2, a)),
+	drop2: await F.collect(F.drop(2, a)),
+	lastN3: await F.lastN(3, a),
 });
 console.log({
 	m,
@@ -75,8 +78,10 @@ console.log({
 	first: await F.first(m),
 	tail: await F.collect(F.tail(m)),
 	last: await F.last(m),
-	take: await F.collect(F.take(2, m)),
-	drop: await F.collect(F.drop(2, m)),
+	firstN3: await F.collect(F.firstN(3, m)),
+	take2: await F.collect(F.take(2, m)),
+	drop2: await F.collect(F.drop(2, m)),
+	lastN3: await F.lastN(3, m),
 });
 console.log({
 	o,
@@ -85,10 +90,12 @@ console.log({
 	keys: await F.collectKeys(o),
 	vals: await F.collectValues(o),
 	first: await F.first(o),
-	tail: await F.collectEntries(F.tail(o)),
+	tail: await F.collect(F.tail(o)),
 	last: await F.last(o),
-	take: await F.collect(F.take(2, o)),
-	drop: await F.collect(F.drop(2, o)),
+	firstN3: await F.collect(F.firstN(3, o)),
+	take2: await F.collect(F.take(2, o)),
+	drop2: await F.collect(F.drop(2, o)),
+	lastN3: await F.lastN(3, o),
 });
 console.log({
 	set,
@@ -97,10 +104,26 @@ console.log({
 	keys: await F.collectKeys(set),
 	vals: await F.collectValues(set),
 	first: await F.first(set),
-	tail: await F.collectEntries(F.tail(set)),
+	tail: await F.collect(F.tail(set)),
 	last: await F.last(set),
-	take: await F.collect(F.take(2, set)),
-	drop: await F.collect(F.drop(2, set)),
+	firstN3: await F.collect(F.firstN(3, set)),
+	take2: await F.collect(F.take(2, set)),
+	drop2: await F.collect(F.drop(2, set)),
+	lastN3: await F.lastN(3, set),
+});
+console.log({
+	str,
+	collectEnum: await F.collect(F.enumerate(str)),
+	entry: await F.collectEntries(str),
+	keys: await F.collectKeys(str),
+	vals: await F.collectValues(str),
+	first: await F.first(str),
+	tail: await F.collect(F.tail(str)),
+	last: await F.last(str),
+	firstN3: await F.collect(F.firstN(3, str)),
+	take2: await F.collect(F.take(2, str)),
+	drop2: await F.collect(F.drop(2, str)),
+	lastN3: await F.lastN(3, str),
 });
 
 // problems: first() is not working; collecting/enumerating for objects is faulty for non-numeric keys and sets show duplicate values
