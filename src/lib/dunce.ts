fada18353b8809d4b451b22edd0884b00d2191ce
Roy Ivy III <rivy.dev@gmail.com>
2021-04-06 23:43:06 -0500

    wip!/UNC ~ add dunce skeleton lib file

---
+++ src/lib/dunce.ts
@@ -0,0 +1,27 @@
+// ref: ['dunce' crate](https://gitlab.com/kornelski/dunce) (aka de-UNC)
+// * see <https://gitlab.com/kornelski/dunce/-/blob/master/src/lib.rs>
+
+const RESERVED_NAMES = [
+	'AUX',
+	'NUL',
+	'PRN',
+	'CON',
+	'COM1',
+	'COM2',
+	'COM3',
+	'COM4',
+	'COM5',
+	'COM6',
+	'COM7',
+	'COM8',
+	'COM9',
+	'LPT1',
+	'LPT2',
+	'LPT3',
+	'LPT4',
+	'LPT5',
+	'LPT6',
+	'LPT7',
+	'LPT8',
+	'LPT9',
+];
