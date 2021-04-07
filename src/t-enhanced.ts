f7029dcb64b3cbd7e7be8f3504400d47e5f15b34
Roy Ivy III <rivy.dev@gmail.com>
2021-04-06 20:41:20 -0500

    x ~ experiment with enhanced running

---
+++ src/t-enhanced.ts
@@ -0,0 +1,20 @@
+// spell-checker:ignore (vars) arr globstar gmsu nullglob PATHEXT
+
+import * as Path from 'https://deno.land/std@0.83.0/path/mod.ts';
+
+import * as Me from './lib/me.ts';
+
+const me = Me.info();
+const meExec = Deno.execPath();
+const meMain = Deno.mainModule;
+
+console.warn(me.name, { meExec, meMain, me });
+
+if (Deno.build.os === 'windows' && !me[0]) {
+	console.warn(
+		me.name +
+			': warn: diminished capacity; full functionality requires an enhanced runner (use `dxr` or install with `dxi`)'
+	);
+}
+
+// Deno.exit(101);
