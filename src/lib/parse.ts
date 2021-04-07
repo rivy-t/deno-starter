db38d6aa110a592dead6195931ee057310aee4e9
Roy Ivy III <rivy.dev@gmail.com>
2021-04-06 23:44:56 -0500

    refactor ~ (parse) rename `splitByBareWS()` from `quotedTokens()`

--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,17 +1,28 @@
-export function quotedTokens(s: string): Array<string> {
+const DQStringReS = '"[^"]*(?:"|$)'; // double-quoted string (unbalanced at end-of-line is allowed)
+const SQStringReS = "'[^']*(?:'|$)"; // single-quoted string (unbalanced at end-of-line is allowed)
+const DQStringStrictReS = '"[^"]*"'; // double-quoted string (quote balance is required)
+const SQStringStrictReS = "'[^']*'"; // single-quoted string (quote balance is required)
+
+const notQWSReS = `(?:(?!["']|\\s).)`; // non-quote, non-whitespace character
+
+export function splitByBareWS(s: string): Array<string> {
 	// parse string into tokens separated by unquoted-whitespace
-	// * supports both single and double quotes; escapes are not supported
+	// * supports both single and double quotes; note: no character escape sequences are recognized
+	const addBalance = true;
 	const arr: Array<string> = [];
 	s.replace(/^\s+/, ''); // trim leading whitespace
-	while (s !== '') {
-		const m = s.match(/^((?:"[^"]*(?:"|$)|'[^']*(?:'|$)|\S+)+)(.*$)/msu);
+	// console.warn({ _: 'splitByBareWS()', s });
+	while (s) {
+		const m = s.match(
+			new RegExp(`^((?:${DQStringReS}|${SQStringReS}|${notQWSReS}+)*)(.*$)`, 'msu')
+		);
 		if (m) {
 			arr.push(m[1]);
 			s = m[2] ? m[2].replace(/^\s+/, '') : ''; // trim leading whitespace
 		} else {
 			s = '';
 		}
-		// console.warn({ from: 'quotedTokens()', s, m, arr });
+		// console.warn({ _: 'splitByBareWS()', s, m, arr });
 	}
 	return arr;
 }
