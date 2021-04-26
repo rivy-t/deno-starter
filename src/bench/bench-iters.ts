470b0de478c5cb27e678c1286e4b08d51ebdc104
Roy Ivy III <rivy.dev@gmail.com>
2021-04-26 00:27:37 -0500

    wip/x ~ futher iter experimentation

--- src/bench/bench-iters.ts
+++ src/bench/bench-iters.ts
@@ -137,86 +137,11 @@ for (let i = 0; i < size; i++) {
 
 log.debug({ arrEg: arr.slice(0, 10) });
 
-import { iterplus as itPlus } from 'https://deno.land/x/iterplus@v2.3.0/index.ts';
-import { Lazy } from 'https://deno.land/x/lazy@v1.7.2/lib/mod.ts';
-
-// ref: <https://github.com/Aplet123/iterplus>
-function itpLongestCommonPrefix(list: Iterable<string>) {
-	const itList = itPlus(list).collect();
-	if (itList.length == 0) return '';
-	if (itList.length == 1) return itList[0];
-	return itPlus(itList[0])
-		.zip(...itList.slice(1))
-		.takeWhile((NthChars) => NthChars.every((c) => c === NthChars[0]))
-		.map((NthChars) => NthChars[0])
-		.collect()
-		.join('');
-}
-
-function isAsyncIter(obj: unknown): obj is AsyncIterator<unknown> {
-	return (
-		typeof obj === 'object' &&
-		obj !== null &&
-		typeof (obj as any).next === 'function'
-	);
-}
-function canAsyncIter(obj: unknown): obj is AsyncIterable<unknown> {
-	return (
-		//typeof obj === "string" ||
-		typeof obj === 'object' && obj !== null && Symbol.asyncIterator in obj
-	);
-}
-
-async function* asyncIt<T>(it: AsyncIterable<T> | Iterable<T>): AsyncIterable<T> {
-	if (canAsyncIter(it)) return it;
-	for (const e of it) {
-		yield e;
-	}
-}
-
-// ref: <https://stackoverflow.com/a/48293566/43774> , <https://stackoverflow.com/questions/4856717/javascript-equivalent-of-pythons-zip-function>
-async function* zip<T>(...iterables: (Iterable<T> | AsyncIterable<T>)[]): AsyncIterable<T[]> {
-	if (!iterables.length) return;
-	const iterators = iterables.map((iterable) => asyncIt(iterable)[Symbol.asyncIterator]());
-	while (true) {
-		const results = await Promise.all(iterators.map((it) => it.next()));
-		if (results.some((res) => res.done)) return;
-		else yield results.map((res) => res.value);
-	}
-}
-function* zipSync<T>(...iterables: Iterable<T>[]): Iterable<T[]> {
-	if (!iterables.length) return;
-	const iterators = iterables.map((iterable) => iterable[Symbol.iterator]());
-	while (true) {
-		const results = iterators.map((it) => it.next());
-		if (results.some((res) => res.done)) return;
-		else yield results.map((res) => res.value);
-	}
-}
-
-function lazyLongestCommonPrefixSync(list: Iterable<string>) {
-	return Lazy.from(zipSync(...list))
-		.where((NthChars) => NthChars.every((c) => c === NthChars[0]))
-		.select((NthChars) => NthChars[0])
-		.toArray()
-		.join('');
-}
-
-async function lazyLongestCommonPrefix(list: Iterable<string> | AsyncIterable<string>) {
-	const itList = await (async () => {
-		if (!isAsyncIter(list)) return list as Iterable<string>;
-		const a = [];
-		for await (const e of list) {
-			a.push(e);
-		}
-		return a;
-	})();
-	return Lazy.from(zipSync(...itList))
-		.where((NthChars) => NthChars.every((c) => c === NthChars[0]))
-		.select((NthChars) => NthChars[0])
-		.toArray()
-		.join('');
-}
+import {
+	itpLongestCommonPrefix,
+	lazyLongestCommonPrefix,
+	lazyLongestCommonPrefixSync,
+} from '../t-iters/t-iters.ts';
 
 bench({
 	name: 'iterplus LongestCommonPrefix',
@@ -254,7 +179,7 @@ bench({
 		return (b: BenchmarkTimer) => {
 			const idx = passN++ % arr.length;
 			b.start();
-			lazyLongestCommonPrefix(arr[idx]);
+			(async () => await lazyLongestCommonPrefix(arr[idx]))();
 			b.stop();
 		};
 	})(),
--- src/t-iters/t-iters.ts
+++ src/t-iters/t-iters.ts
@@ -7,7 +7,7 @@ import { iterplus as itPlus } from 'https://deno.land/x/iterplus@v2.3.0/index.ts
 import { Lazy } from 'https://deno.land/x/lazy@v1.7.2/lib/mod.ts';
 
 // ref: <https://github.com/Aplet123/iterplus>
-function itpLongestCommonPrefix(list: Iterable<string>) {
+export function itpLongestCommonPrefix(list: Iterable<string>) {
 	const itList = itPlus(list).collect();
 	if (itList.length == 0) return '';
 	if (itList.length == 1) return itList[0];
@@ -19,35 +19,81 @@ function itpLongestCommonPrefix(list: Iterable<string>) {
 		.join('');
 }
 
+export function isAsyncIter(obj: unknown): obj is AsyncIterator<unknown> {
+	return (
+		typeof obj === 'object' &&
+		obj !== null &&
+		// deno-lint-ignore no-explicit-any
+		typeof (obj as any).next === 'function'
+	);
+}
+export function canAsyncIter(obj: unknown): obj is AsyncIterable<unknown> {
+	return (
+		typeof obj === 'object' && obj !== null && Symbol.asyncIterator in obj
+	);
+}
+
+export async function* asyncIt<T>(it: AsyncIterable<T> | Iterable<T>): AsyncIterable<T> {
+	if (canAsyncIter(it)) return it;
+	for (const e of it) {
+		yield e;
+	}
+}
+
 // ref: <https://stackoverflow.com/a/48293566/43774> , <https://stackoverflow.com/questions/4856717/javascript-equivalent-of-pythons-zip-function>
-function* zip<T>(...iterables: Iterable<T>[]): Iterable<T[]> {
+export async function* zip<T>(
+	...iterables: (Iterable<T> | AsyncIterable<T>)[]
+): AsyncIterable<T[]> {
 	if (!iterables.length) return;
-	const iterators = iterables.map((i) => i[Symbol.iterator]());
+	const iterators = iterables.map((iterable) => asyncIt(iterable)[Symbol.asyncIterator]());
 	while (true) {
-		const results = iterators.map((iter) => iter.next());
+		const results = await Promise.all(iterators.map((it) => it.next()));
 		if (results.some((res) => res.done)) return;
 		else yield results.map((res) => res.value);
 	}
 }
-function lazyLongestCommonPrefix(list: Iterable<string>) {
-	return Lazy.from(zip(...list))
+export function* zipSync<T>(...iterables: Iterable<T>[]): Iterable<T[]> {
+	if (!iterables.length) return;
+	const iterators = iterables.map((iterable) => iterable[Symbol.iterator]());
+	while (true) {
+		const results = iterators.map((it) => it.next());
+		if (results.some((res) => res.done)) return;
+		else yield results.map((res) => res.value);
+	}
+}
+
+export function lazyLongestCommonPrefixSync(list: Iterable<string>) {
+	return Lazy.from(zipSync(...list))
 		.where((NthChars) => NthChars.every((c) => c === NthChars[0]))
 		.select((NthChars) => NthChars[0])
 		.toArray()
 		.join('');
 }
 
-const data = ['flowery', 'floral', 'flux', 'flannel'];
-
-console.log('itpLongestCommonPrefix', itpLongestCommonPrefix(data));
-console.log('lazyLongestCommonPrefix', lazyLongestCommonPrefix(data));
-console.log('itpLongestCommonPrefix', itpLongestCommonPrefix([]));
-console.log('lazyLongestCommonPrefix', lazyLongestCommonPrefix([]));
-
-async function* asyncList(list: string[]) {
-	for (let idx = 0; idx < list.length; idx++) {
-		yield list[idx];
-	}
+export async function lazyLongestCommonPrefix(list: Iterable<string> | AsyncIterable<string>) {
+	const itList = await (async () => {
+		if (!isAsyncIter(list)) return list as Iterable<string>;
+		const a: string[] = [];
+		for await (const e of list) {
+			a.push(e);
+		}
+		return a;
+	})();
+	return Lazy.from(zipSync(...itList))
+		.where((NthChars) => NthChars.every((c) => c === NthChars[0]))
+		.select((NthChars) => NthChars[0])
+		.toArray()
+		.join('');
 }
 
-// console.log('lazyLongestCommonPrefix', lazyLongestCommonPrefix(asyncList(data)));
+// if (Deno.mainModule === import.meta.url) {
+if (import.meta.main) {
+	const data = ['flowery', 'floral', 'flux', 'flannel'];
+	console.log('itpLongestCommonPrefix', itpLongestCommonPrefix(data));
+	console.log('lazyLongestCommonPrefixSync', lazyLongestCommonPrefixSync(data));
+	console.log('lazyLongestCommonPrefix', await lazyLongestCommonPrefix(data));
+	console.log('lazyLongestCommonPrefix', await lazyLongestCommonPrefix(asyncIt(data)));
+	console.log('itpLongestCommonPrefix', itpLongestCommonPrefix([]));
+	console.log('lazyLongestCommonPrefixSync', lazyLongestCommonPrefixSync([]));
+	console.log('lazyLongestCommonPrefix', await lazyLongestCommonPrefix([]));
+}
