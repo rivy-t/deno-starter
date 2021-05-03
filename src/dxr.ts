3ceb30c6910f44615773f2191e7d69f72229d994
Roy Ivy III <rivy.dev@gmail.com>
2021-05-02 22:55:32 -0500

    refactor ~ polish `dxr` code

--- src/dxr.ts
+++ src/dxr.ts
@@ -1,10 +1,4 @@
-// spell-checker:ignore (vars) arr globstar gmsu nullglob PATHEXT
-
-import * as Path from 'https://deno.land/std@0.83.0/path/mod.ts';
-import { exists, existsSync } from 'https://deno.land/std@0.92.0/fs/exists.ts';
-import { expandGlob, expandGlobSync } from 'https://deno.land/std@0.92.0/fs/expand_glob.ts';
-import { walk, walkSync } from 'https://deno.land/std@0.92.0/fs/walk.ts';
-const fs = { exists, existsSync, expandGlob, expandGlobSync, walk, walkSync };
+// spell-checker:ignore (vars) arr gmsu
 
 import * as xArgs from './lib/xArgs.ts';
 import * as Me from './lib/xProcess.ts';
@@ -19,24 +13,17 @@ if (Deno.build.os === 'windows' && !Me.arg0) {
 	);
 }
 
-// export type ArgIncrement = { arg: string; tailOfArgExpansion: AsyncIterableIterator<string>[]; tailOfArgsText: string; };
-// export type ArgIncrementSync = { arg: string; tailOfArgExpansion: string[][]; tailOfArgsText: string };
 const { arg: targetPath, tailOfArgExpansion, tailOfArgsText } = await (async () => {
 	const it = xArgs.argsIt(Me.argsText || '');
 	const itNext = await it.next();
 	return !itNext.done ? itNext.value : { arg: '', tailOfArgExpansion: [], tailOfArgsText: '' };
 })();
 
-// console.warn({ targetPath, CWD: Deno.cwd() });
+// console.warn(Me.name, { targetPath, CWD: Deno.cwd() });
 if (!targetPath) {
 	console.error(`${Me.name}: err!: no target name supplied (use \`${Me.name} TARGET\`)`);
 	Deno.exit(1);
 } else {
-	// console.warn(Me.name, { targetPath });
-	// if (!fs.existsSync(targetPath)) {
-	// 	console.error(`${Me.name}: err!: target ('${targetPath}') does not exist`);
-	// 	Deno.exit(1);
-	// }
 	const iteratedArgTail = (await Promise.all(tailOfArgExpansion.flatMap(async (it) => {
 		const arr: string[] = [];
 		for await (const a of it) arr.push(a);
@@ -44,13 +31,13 @@ if (!targetPath) {
 	}))).flat();
 	// console.warn(Me.name, { tailOfArgExpansion, iteratedArgTail });
 
-	let shimURL;
-	// try {
-	shimURL = (new URL(targetPath, 'file://' + Deno.cwd() + '/')).href;
-	// } catch {
-	// 	shimURL = '';
-	// }
-	console.warn('dxr', { CWD: Deno.cwd(), targetPath, shimURL });
+	let targetURL;
+	try {
+		targetURL = (new URL(targetPath, 'file://' + Deno.cwd() + '/')).href;
+	} catch {
+		targetURL = '';
+	}
+	console.warn(Me.name, { CWD: Deno.cwd(), targetPath, targetURL });
 
 	const targetArgs = [
 		...iteratedArgTail,
@@ -65,7 +52,7 @@ if (!targetPath) {
 		env: {
 			DENO_SHIM_ARG0: `${Me.arg0 ? Me.arg0 : ['deno', ...denoOptions].join(' ')} ${targetPath}`,
 			DENO_SHIM_ARGS: targetArgs,
-			DENO_SHIM_URL: shimURL,
+			DENO_SHIM_URL: targetURL,
 		},
 	};
 	console.warn(Me.name, { runOptions });
